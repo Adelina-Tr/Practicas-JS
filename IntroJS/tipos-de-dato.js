9e56c0df91fda967692ea1aafa5ee8e6c4e763f9
Adelina Trejo <kf8@hotmail.com>
2025-02-28 13:48:41 -0600

    Practica 1 - Introduccion a JS

---
+++ IntroJS/tipos-de-dato.js
@@ -0,0 +1,82 @@
+
+//Tipos de Dato Numericos
+let varNum1 = 42;
+let varNum2 = -666;
+let varNum3 = 0;
+let varNum4 = 3.1416;
+let varNum5 = NaN;
+
+//Tipos de Dato String
+let varStr1 = 'Veinticinco';
+let varStr2 = "Adelina";
+let varStr3 = `Adelina`;
+let varStr4 = 'A'
+
+//Tipos de Dato Boolean
+let varBool1 = true;
+let varBool2 = false;
+
+//Tipos de Dato Especiales
+let varEspecial = null;
+let varEspecial2 = undefined;
+
+//Tipos de Dato Simbol y BigInt
+let varSymbol = Symbol("id");
+let varBigInt = 12482403502347607340n;
+
+//Tipo de dato Objeto
+let fecha = new Date();
+let numeros = [1,2,3,4,5];
+let persona = {
+    nombre: "Adelina",
+    edad: 26,
+    nacionalidad: "Mexicano"
+}
+
+//Tipo de dato Funcion
+function suma(a,b) { 
+    return a + b
+}
+
+
+//Imprimir los Tipos de Dato
+console.log(typeof varNum1);
+console.log(typeof varNum2);
+console.log(typeof varNum3);
+console.log(typeof varNum4);
+console.log(typeof varNum5);
+console.log(typeof varStr1);
+console.log(typeof varStr2);
+console.log(typeof varStr3);
+console.log(typeof varStr4);
+console.log(typeof varBool1);
+console.log(typeof varBool2);
+console.log(typeof varEspecial);
+console.log(typeof varEspecial2);
+console.log(typeof varSymbol);
+console.log(typeof varBigInt);
+console.log(typeof fecha);
+console.log(typeof numeros);
+console.log(typeof persona);
+console.log(typeof suma);
+
+//Mostrar los Tipos de Dato en Consola
+console.log(varNum1);
+console.log(varNum2);       
+console.log(varNum3);
+console.log(varNum4);
+console.log(varNum5);
+console.log(varStr1);
+console.log(varStr2);
+console.log(varStr3);
+console.log(varStr4);
+console.log(varBool1);
+console.log(varBool2);
+console.log(varEspecial);
+console.log(varEspecial2);
+console.log(varSymbol);
+console.log(varBigInt);
+console.log(fecha);
+console.log(numeros);
+console.log(persona);
+console.log(suma);
